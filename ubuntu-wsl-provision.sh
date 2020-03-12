90726fdf578592ee2f68cccfa6876eab5cd53bf6
Rj Rybarczyk <rj@rybar.tech>
2020-03-12 13:49:06 -0700

    Add wsl ubuntu related files

--- ubuntu-wsl-provision.sh
+++ ubuntu-wsl-provision.sh
@@ -1,46 +1,37 @@
-# Exit script upon fail, error if variable undefined, print each command
-set -euxo pipefail
+#  Error if variable undefined, print each command
+# set -euf -o pipefile
 
 # Update installed packages
-sudo pacman -Syu --noconfirm
+sudo apt -y update
+sudo apt -y full-upgrade
 
 # Install packages if they do not already exist, default "y"
-sudo pacman -S --noconfirm --needed tmux git dhcpcd networkmanager tree ngrep openssl ufw pkg-config base-devel openssh netcat mkpkg
 
-# Wayland Enviornment
-sudo pacman -S wayland wayland-protocols meson xorg-server-xwayland egl-wayland libdrm gbm libinput xkbcommon pixman
+# General Enviornment Dev Tools
+sudo apt install -y build-essential llvm libssl-dev pkg-config python3 python-pip3 ruby-full 
+sudo apt install -y tmux git tree ngrep
 
-# Sway Windows Manager Enviornment
-sudo pacman -S --noconfirm --needed sway swayidle swaylock rofi slurp grim
-
-# General Enviornment Tools
-sudo pacman -S --noconfirm --needed unzip clang feh firefox okular alacritty lolcat alsa-utils lastpass-cli watchexec
-
-sudo pacman -S --noconfirm --needed dbus bluez bluez-utils pulseaudio-bluetooth alsa-plugins pulseaudio pulseaudio-alsa ponymix
-
-# LaTeX Development
-sudo pacman -S --noconfirm --needed texlive-most biber r
+# Niceities
+sudo apt install -y lolcat 
 
 # Install Rust
-curl -sSf https://sh.rustup.rs | sh
+curl -sSf https://sh.rustup.rs | sh | sh -s -- -y
+source $HOME/.cargo/bin
+
+# Install useful rustup tools and global crates
+rustup install nightly
+rustup component add rustfmt
+rustup component add clippy
+
+cargo install ripgrep fd-find just bat exa qc cargo-watch cargo-check cargo-outdated cargo-edit
 
 # DotBot Setup
-git clone https://github.com/rrybarczyk/dots.git ~/.dots
-cd ~/.dots && git submodule update --init --recursive .
-$(SHELL)=sh ./submodules/dotbot/bin/dotbot -c default.yaml
+git submodule update --init --recursive .
+./submodules/dotbot/bin/dotbot -c default.yaml
 
 # Force symlink bash scripts
-ln -sf ~/.dots/etc/bashrc ~/.bashrc
-ln -sf ~/.dots/etc/bash_profile ~/.bash_profile
-ln -sf ~/.dots/etc/profile ~/.profile
+# ln -sf ~/.dots/etc/bashrc ~/.bashrc
+# ln -sf ~/.dots/etc/bash_profile ~/.bash_profile
+# ln -sf ~/.dots/etc/profile ~/.profile
 source ~/.bashrc
 
-# Install yay package manager
-cd ~/aur && git clone https://aur.archlinux.org/yay.git
-cd ~/aur/yay && makepkg -sic
-cd ~
-
-# Install aur packages
-yay -S --needed --noconfirm signal-desktop-bin
-
-yay -Syu
