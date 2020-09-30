0203961d5607f904757c9be3c9db6ccdce32a956
RJ Rybarczyk <rj@rybar.tech>
2020-09-30 14:20:02 +0000

    Add vulkan to provision, use yay exclusively, which-gpu cmd in just

--- provision.sh
+++ provision.sh
@@ -5,19 +5,27 @@ set -euxo pipefail
 # Update installed packages
 sudo pacman -Syu --noconfirm
 
+# Install yay package manager
+mkdir -p ~/aur
+cd ~/aur && git clone https://aur.archlinux.org/yay.git
+cd ~/aur/yay && makepkg -sic
+cd ~
+
 # Install packages if they do not already exist, default "y"
-sudo pacman -S --noconfirm --needed tmux git dhcpcd networkmanager openssl ufw pkg-config base-devel intelucode openssh mkpkg clang
+yay -S --noconfirm --needed tmux git dhcpcd networkmanager openssl ufw pkg-config base-devel intelucode openssh mkpkg clang
 
 # X and  i3 Windows Manager Enviornment
-sudo pacman -S --noconfirm --needed xorg-server xorg-xinit dmenu i3status i3-gaps slock xclip xorg-xeyes
+yay -S --noconfirm --needed xorg-server xorg-xinit dmenu i3status i3-gaps slock xclip xorg-xeyes
 
 # General Enviornment Tools
-sudo pacman -S --noconfirm --needed unzip clang feh firefox okular alacritty lolcat alsa-utils lastpass-cli watchexec wget socat netcat unzip ngrep tree inkscape
+yay -S --noconfirm --needed unzip clang feh firefox okular alacritty lolcat alsa-utils lastpass-cli watchexec wget socat netcat unzip ngrep tree inkscape
+yay -S --noconfirm --needed dbus bluez bluez-utils pulseaudio-bluetooth alsa-plugins pulseaudio pulseaudio-alsa ponymix
 
-sudo pacman -S --noconfirm --needed dbus bluez bluez-utils pulseaudio-bluetooth alsa-plugins pulseaudio pulseaudio-alsa ponymix
+# Vulkan
+yay -S --noconfirm --needed vulkan-tools vulkan-dev vulkan-intel
 
 # LaTeX Development
-sudo pacman -S --noconfirm --needed texlive-most biber r
+yay -S --noconfirm --needed texlive-most biber r
 
 # Install Rust
 curl -sSf https://sh.rustup.rs | sh
@@ -35,11 +43,6 @@ cd ~/.dots && just
 
 # Force symlink bash scripts
 source ~/.bashrc
-
-# Install yay package manager
-cd ~/aur && git clone https://aur.archlinux.org/yay.git
-cd ~/aur/yay && makepkg -sic
-cd ~
 
 # Install aur packages
 # Chat apps
