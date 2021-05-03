20430bd8274cefe297b7acb001327048cf498376
Rj Rybarczyk <rj@rybar.tech>
2021-05-03 19:20:52 +0000

    Profile and env setup

--- provision.sh
+++ provision.sh
@@ -11,6 +11,15 @@ sudo apt -y full-upgrade
 sudo apt install -y build-essential llvm libssl-dev pkg-config cmake python python3 python-pip3 ruby-full
 sudo apt install -y tmux git tree ngrep 
 
+### Begin Bitcoin Build Packages
+sudo apt install -y build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
+sudo apt install -y libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev
+# SQLite is requited for descriptor wallet
+sudo apt install -y libsqlite3-dev
+# ZMQ dependencies (provides ZMQ API)
+sudo apt -y install libzmq3-dev
+### End Bitcoin Build Packages
+
 mv ~/.bashrc ~/.bashrc.bk
 mv ~/.profile ~/.profile.bk
 mv ~/.bash_profile ~/.bash_profile.bk
@@ -20,8 +29,8 @@ git submodule update --init --recursive .
 ./submodules/dotbot/bin/dotbot -c default.yaml
 
 # Install Rust
-curl -sSf https://sh.rustup.rs | sh | sh -s -- -y
-source $HOME/.cargo/bin
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
 rustup install nightly
 rustup component add rustfmt
 rustup component add clippy
@@ -37,12 +46,9 @@ rustup component add clippy
 # just              - a better make <3
 # qc                - an advanced RPN cli calculator
 # ripgrep           - a better grep
-# xsv               - fast csv cli toolkit
 # cargo-check       - checks for rust code errors
 # cargo-edit        - add, remove, upgrade dependencies from cli
-# cargo-flamegraph  - generate flamegraphs and profiling data
 # cargo-outdated    - indicates when Rust dependencies are out of date
 # cargo-watch       - reload cargo commands on file save
-cargo install bat exa fd-find just qc ripgrep xsv cargo-check cargo-edit cargo-flamegraph cargo-outdated cargo-watch
+cargo install bat exa fd-find just qc ripgrep cargo-check cargo-edit cargo-outdated cargo-watch watchexec
 source ~/.bashrc
-
