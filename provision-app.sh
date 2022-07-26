c4a6f1f9d3fa9cd5b5db282d667c40f1442b2236
Rachel Rybarczyk <rachelrybarczyk@gmail.com>
2022-07-26 18:42:27 +0000

    Add rust-analyzer

--- provision-app.sh
+++ provision-app.sh
@@ -4,3 +4,10 @@
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -P ~/.dots/app/nvim
 cd ~/.dots/app/nvim
 tar xzvf nvim-linux64.tar.gz
+
+# Install rust-analyzer
+wget https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz -P ~/.dots/app/rust-analyzer
+cd ~/.dots/app/rust-analyzer
+gzip rust-analyzer-x86_64-unknown-linux-gnu.gz -d
+mv rust-analyzer-x86_64-unknown-linux-gnu rust-analyzer
+chmod +x ~/.dots/app/rust-analyzer/rust-analyzer
