81b6d0ee5895ccf86d0272941943eaaf3885e830
Rachel Rybarczyk <rachel.rybarczyk@galaxy.com>
2023-09-13 18:50:18 +0000

    Clean

--- 0-provision-env.sh
+++ 0-provision-env.sh
@@ -1,8 +1,8 @@
 #  Error if variable undefined, print each command
 # set -euf -o pipefile
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 sudo apt install zsh
 # Needs reboot to use zsh
@@ -17,41 +17,30 @@ sudo apt install cmake pkg-config libssl-dev libglib2.0-dev gcc-multilib -y
 sudo apt install -y libappindicator1 fonts-liberation
 sudo apt install -f
 
-# To get diesel_cli working for mysql
-# sudo apt-get install libmysqlclient-dev libpq-dev libmariadbclient-dev-compat libsqlite3-dev libmysqlclient-dev -y
-
-# XCB Library for Interoperability with Xlib
-# sudo apt install libxcb-shape0-dev libxcb-xfixes0-dev -y
-
 # Bitcoin build
 # sudo apt install autoconf libboost-all-dev libevent-dev -y
 # sudo apt-get install libdb++-dev -y
 
-# Install python
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update -y
+# # Install python
+# sudo apt install software-properties-common
+# sudo add-apt-repository ppa:deadsnakes/ppa
+# sudo apt update -y
 sudo apt install python3-pip -y
 sudo apt install flake8 -y
-# sudo apt-get install python3-venv
+sudo apt install python3.8-venv -y
+pip install pyright  # nvim-lspconfig dependency
 
 # Poetry env install: https://python-poetry.org/docs/#installing-with-the-official-installer
 curl -sSL https://install.python-poetry.org | python3 -
-
 
 # Install neovim
 sudo apt install libtool automake libncurses5-dev g++ -y
 sudo apt install neovim
 
-# nvim-lspconfig dependency
-sudo npm i -g pyright
-
 # Niceities
 sudo apt install tldr wget -y
-
-sudo apt install fzf -y
 # Better search to supplement fzf vim plugin
-sudo apt install silversearcher-ag -y
+sudo apt install fzf silversearcher-ag -y
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -84,6 +73,9 @@ rustup component add rust-src
 cargo install bat exa fd-find just qc quick-calc ripgrep cargo-check cargo-edit cargo-outdated cargo-watch watchexec-cli
 # Extra cargo pacakges
 # cargo install xsv cargo-flamegraph diesel_cli
+# To get diesel_cli working for mysql
+# sudo apt-get install libmysqlclient-dev libpq-dev libmariadbclient-dev-compat libsqlite3-dev libmysqlclient-dev -y
+# cargo install diesel_cli
 
 # Set global git username + email
 git config --global user.name "Rachel Rybarczyk"
