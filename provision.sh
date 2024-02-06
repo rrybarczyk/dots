a76d0e8cea7e3ee8050a8e4bf8ea35c5a1b80687
Rachel Rybarczyk <rachel.rybarczyk@gmail.com>
2024-02-06 16:56:30 -0500

    Update provision for Apple Silicon + neovim support

--- provision.sh
+++ provision.sh
@@ -2,32 +2,34 @@
 # Exit script upon fail, error if variable undefined, print each command
 set -euxo pipefail
 
-# Set git credentials
+# # Set git credentials
 git config --global user.name "Rachel Rybarczyk"
 git config --global user.email "rachel.rybarczyk@gmail.com"
 
-## Install brew package manger
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-export PATH=/opt/homebrew/bin:$PATH
-export PATH=$PATH:/Users/rachel/.local/bin/nvim-macos/bin
-brew install zsh lspconfig tmux wget htop tree lolcat watchexec tmux-mem-cpu-load
-# brew install imagemagick gnu-time gnupg2 git-lfs
-# brew install rust-analyzer
-# brew cask install mactex-no-gui
-# brew cask install ngrok
-# brew cask install wireshark
+## Install brew package manger + add to path
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# # For Intel Macs
+# export PATH=/opt/homebrew/bin:$PATH
+# For Apple Silicon Macs
+export PATH="/opt/homebrew/bin:$PATH"
 
-## Install Oh My Zsh
+brew install neovim tmux wget htop tree lolcat watchexec tmux-mem-cpu-load
+brew install imagemagick gnu-time gnupg2 git-lfs
+brew install rust-analyzer
+brew cask install mactex-no-gui
+brew cask install ngrok
+brew cask install wireshark
+
+## Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install zsh-syntax-highlighting in oh-my-zsh plugins directory
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-### Install 3rd Party Binaries
-mkdir -p ~/.local/bin
 
-## Install neovim
-wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-macos.tar.gz -P ~/.local/bin
-xattr -c ~/.local/bin/nvim-macos.tar.gz
-tar xzvf ~/.local/bin/nvim-macos.tar.gz -C ~/.local/bin
-rm -rf ~/.local/bin/nvim-macos.tar.gz
+## Install Plug for neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 ## Install Rust
 curl -sSf https://sh.rustup.rs | sh
@@ -35,19 +37,20 @@ source ~/.profile
 rustup install nightly
 rustup component add rustfmt
 rustup component add clippy
-
-bat               - a colorful cat
-exa               - a colorful ls
-fd-find           - a better find
-hexyl             - hex dump
-just              - a better make <3
-qc                - an advanced RPN cli calculator
-ripgrep           - a better grep
-xsv               - fast csv cli toolkit
-cargo-check       - checks for rust code errors
-cargo-edit        - add, remove, upgrade dependencies from cli
-flamegraph  - generate flamegraphs and profiling data
-cargo-outdated    - indicates when Rust dependencies are out of date
-cargo-watch       - reload cargo commands on file save
+#
+# bat               - a colorful cat
+# exa               - a colorful ls
+# fd-find           - a better find
+# hexyl             - hex dump
+# just              - a better make <3
+# qc                - an advanced RPN cli calculator
+# ripgrep           - a better grep
+# xsv               - fast csv cli toolkit
+# cargo-check       - checks for rust code errors
+# cargo-edit        - add, remove, upgrade dependencies from cli
+# flamegraph  - generate flamegraphs and profiling data
+# cargo-outdated    - indicates when Rust dependencies are out of date
+# cargo-watch       - reload cargo commands on file save
 cargo install just fd-find ripgrep qc
-# cargo install bat exa hexyl xsv cargo-check cargo-edit cargo-outdated cargo-watch
+cargo install bat exa hexyl
+cargo install cargo-check cargo-edit cargo-outdated cargo-watch
